6fbfc7bade857fdca5a2a5faa77337882fb7babb
Cadu <ceblpm@cin.ufpe.br>
2021-06-01 14:44:49 -0300

    Link User entity to typeorm

--- src/modules/users/entities/User.ts
+++ src/modules/users/entities/User.ts
@@ -1,16 +1,24 @@
 import { v4 as uuid } from 'uuid'
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
 
+@Entity('users')
 class User {
+  @PrimaryColumn()
   id: string
 
+  @Column()
   name: string
 
+  @Column()
   email: string
 
+  @Column()
   password: string
 
+  @CreateDateColumn()
   created_at: Date
 
+  @CreateDateColumn()
   updated_at: Date
 
   constructor() {
