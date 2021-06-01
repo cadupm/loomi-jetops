1738bd1be29f415636d9731ddbfbbe02e341e851
Cadu <ceblpm@cin.ufpe.br>
2021-06-01 15:36:09 -0300

    Creating end-point to list all users and building the method listAllUsers at UsersRepository

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,5 +6,8 @@ const usersRoutes = Router()
 const createUserController = new CreateUserController()
 
 usersRoutes.post('/', createUserController.handle)
+usersRoutes.get('/', (request, response) => {
+  return response.json({ msg: 'Link to end-point list users is done'})
+})
 
 export { usersRoutes }
