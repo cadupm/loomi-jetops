b8ad19874aa448dbf1ede2d0101418dc5a285dc9
Cadu <ceblpm@cin.ufpe.br>
2021-06-01 15:06:11 -0300

    Editing column id at migrations to specify that it is a primary key

--- src/database/migrations/1622563300114-CreateUser.ts
+++ src/database/migrations/1622563300114-CreateUser.ts
@@ -9,7 +9,8 @@ export class CreateUser1622563300114 implements MigrationInterface {
                 columns: [
                     {
                         name: 'id',
-                        type: 'uuid'
+                        type: 'uuid',
+                        isPrimary: true
                     },
                     {
                         name: 'name',
