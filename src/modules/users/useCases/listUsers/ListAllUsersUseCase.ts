c101521f3a824091d1f129b5eda9a63a29f39a38
Cadu <ceblpm@cin.ufpe.br>
2021-06-01 15:40:07 -0300

    Creating ListAllUsersUseCase

---
+++ src/modules/users/useCases/listUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,17 @@
+import { injectable, inject } from "tsyringe";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class ListAllUsersUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute(): Promise<User[]> {
+    return await this.usersRepository.listAllUsers()
+  }
+}
+
+export { ListAllUsersUseCase }
