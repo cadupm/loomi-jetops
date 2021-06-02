1e0081fe7d888951aeee51b789e1ab26f141d18a
Cadu <ceblpm@cin.ufpe.br>
2021-06-02 17:04:17 -0300

    Creating ListAircraftsUseCase

---
+++ src/modules/aircrafts/useCases/listAircrafts/ListAircraftsUseCase.ts
@@ -0,0 +1,19 @@
+import { inject, injectable } from "tsyringe";
+import { Aircraft } from "../../entities/Aircraft";
+import { IAircraftsRepository } from "../../repositories/IAircrafsRepository";
+
+@injectable()
+class ListAircraftsUseCase {
+  constructor(
+    @inject('AircraftsRepository')
+    private aircraftsRepository: IAircraftsRepository
+  ) {}
+
+  async execute(): Promise<Aircraft[]> {
+    const aircrafts = await this.aircraftsRepository.listAllAircrafts()
+
+    return aircrafts
+  }
+}
+
+export { ListAircraftsUseCase }
