0ec58fa0ed8a70db49dce06d744247415953eb69
Cadu <ceblpm@cin.ufpe.br>
2021-06-02 15:45:06 -0300

    Creating aircraft entity

---
+++ src/modules/aircrafts/Aircraft.ts
@@ -0,0 +1,26 @@
+import { v4 as uuid } from 'uuid'
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+
+@Entity('aircrafts')
+class Aircraft {
+  @PrimaryColumn()
+  id: string 
+
+  @Column()
+  name: string
+  
+  @Column()
+  type: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuid()
+    }
+  }
+
+}
+
+export { Aircraft }
