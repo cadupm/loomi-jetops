ed6ac1575c259738414e72e2076407c6612ce895
Cadu <ceblpm@cin.ufpe.br>
2021-06-02 16:12:03 -0300

    Creating CreateAircrafController and linking it to end-point

---
+++ src/modules/aircrafts/useCases/CreateAircraftController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+import { CreateAircraftUseCase } from './CreateAircraftUseCase'
+
+class CreateAircraftController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, type } = request.body
+
+    const createAircraftUseCase = container.resolve(CreateAircraftUseCase)
+
+    await createAircraftUseCase.execute({
+      name,
+      type
+    })
+
+    return response.status(201).send()
+  }
+}
+
+export { CreateAircraftController }
--- src/routes/aircrafts.routes.ts
+++ src/routes/aircrafts.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
+import { CreateAircraftController } from '../modules/aircrafts/useCases/CreateAircraftController'
 
 const aircraftsRoutes = Router()
 
+const createAircraftController = new CreateAircraftController()
+
 aircraftsRoutes.use(ensureAuthenticated)
 
-aircraftsRoutes.post('/', (request, response) => {
-  return response.json('Created end-point to create an aircraft by user admin')
-})
+aircraftsRoutes.post('/', createAircraftController.handle)
 
 export { aircraftsRoutes }
