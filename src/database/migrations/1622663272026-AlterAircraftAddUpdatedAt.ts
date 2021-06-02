5384a040890b763fff34f4b446151d9a68e3edbe
Cadu <ceblpm@cin.ufpe.br>
2021-06-02 16:52:11 -0300

    Creating migration to add column updatedAt and refactoring aircraft entity

---
+++ src/database/migrations/1622663272026-AlterAircraftAddUpdatedAt.ts
@@ -0,0 +1,18 @@
+import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+
+export class AlterAircraftAddUpdatedAt1622663272026 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn('aircrafts',
+        new TableColumn({
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('aircrafts', 'updated_at')
+    }
+
+}
--- src/modules/aircrafts/entities/Aircraft.ts
+++ src/modules/aircrafts/entities/Aircraft.ts
@@ -15,6 +15,9 @@ class Aircraft {
   @CreateDateColumn()
   created_at: Date
 
+  @CreateDateColumn()
+  updated_at: Date
+
   constructor() {
     if(!this.id) {
       this.id = uuid()
