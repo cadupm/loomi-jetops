35ba801743278db05139e7aa44e9c23a3d164e1a
Cadu <ceblpm@cin.ufpe.br>
2021-06-02 17:08:26 -0300

    Creating ListAircraftsController and linking it to respect end-point

--- src/routes/aircrafts.routes.ts
+++ src/routes/aircrafts.routes.ts
@@ -1,19 +1,19 @@
 import { Router } from 'express'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 import { CreateAircraftController } from '../modules/aircrafts/useCases/createAircraft/CreateAircraftController'
+import { ListAircraftsController } from '../modules/aircrafts/useCases/listAircrafts/ListAircraftsController'
 import { UpdateAircraftController } from '../modules/aircrafts/useCases/updateAircraft/UpdateAircraftController'
 
 const aircraftsRoutes = Router()
 
 const createAircraftController = new CreateAircraftController()
 const updateAircraftController = new UpdateAircraftController()
+const listAircraftsController = new ListAircraftsController()
 
 aircraftsRoutes.use(ensureAuthenticated)
 
 aircraftsRoutes.post('/', createAircraftController.handle)
-aircraftsRoutes.get('/', (request, response) => {
-  return response.json({ msg: 'End-point connected to list all aircrafts'})
-})
+aircraftsRoutes.get('/', listAircraftsController.handle)
 aircraftsRoutes.patch('/:id', updateAircraftController.handle) 
 
 export { aircraftsRoutes }
